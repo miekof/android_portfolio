b6eddd590ca1376b112bd7906f8bd3f90d44d8fc
burrowqueen <moc.mieko@okeim.com>
2016-01-17 15:04:54 -0600

    Remove commented out code.

--- app/src/main/java/com/okeim/myappportfolio/MainActivity.java
+++ app/src/main/java/com/okeim/myappportfolio/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
         Button buttonSpotify = (Button) findViewById(R.id.button_spotify);
         buttonSpotify.setOnClickListener(new View.OnClickListener() {
